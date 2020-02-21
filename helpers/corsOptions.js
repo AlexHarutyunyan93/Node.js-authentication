fde367d2d7437b0aec0e3f31836c324ac75e48dc
alex.harutyunyan <alex.harutyunyan@synisys.com>
2020-02-21 12:55:08 +0400

    bugfix

--- helpers/corsOptions.js
+++ helpers/corsOptions.js
@@ -1,4 +1,4 @@
-const whitelist = ["http://localhost:3000"];
+const whitelist = ["http://localhost:9090"];
 const corsOptions = {origin: (origin, callback) => {
         if(whitelist.indexOf(origin) != -1) {
             callback(null, true)
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(jwt());
 
