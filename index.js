ba71ffe62e476acfb1e84804837aa0249039739a
alex.harutyunyan <alex.harutyunyan@synisys.com>
2020-02-21 16:05:05 +0400

    global changes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 ﻿const express      = require("express");
-const mongoose     = require('mongoose');
 const bodyParser   = require('body-parser');
 const cors         = require('cors');
 const corsOptions  = require("./helpers/corsOptions");
@@ -9,21 +8,19 @@ const errorHandler = require('./helpers/error-handler');
 const app = express();
 
 
-mongoose.connect('mongodb://localhost/nodejsauth', { useUnifiedTopology: true });
-
-
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 
 app.use(jwt());
 
 //app.use('/api', require('./routes/route'));
-app.use('/users', require('./users/users.controller'));
+app.use('/users', require('./users/user.controller'));
 app.use(errorHandler);
 
-app.listen(7070, () => {
-    console.log("server is on port 7070");
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 7070;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
 });
