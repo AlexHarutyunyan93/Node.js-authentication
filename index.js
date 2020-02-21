b54d21601c661aebe5100f444084d07ef3e5ea7e
alex.harutyunyan <alex.harutyunyan@synisys.com>
2020-02-21 11:13:30 +0400

    authentication

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-const express     = require("express");
-const mongoose    = require('mongoose');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
-const corsOptions = require("./helpers/corsOptions");
+﻿const express      = require("express");
+const mongoose     = require('mongoose');
+const bodyParser   = require('body-parser');
+const cors         = require('cors');
+const corsOptions  = require("./helpers/corsOptions");
+const jwt          = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 
 const app = express();
+
 
 mongoose.connect('mongodb://localhost/nodejsauth', { useUnifiedTopology: true });
 
 
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/api', require('./routes/route'));
+//app.use(cors());
 
+app.use(jwt());
+
+//app.use('/api', require('./routes/route'));
+app.use('/users', require('./users/users.controller'));
+app.use(errorHandler);
 
 app.listen(7070, () => {
     console.log("server is on port 7070");
